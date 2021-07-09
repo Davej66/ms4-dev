34fde291a83d8a538396b03295aca2b72aedee59
bradleyhc <bradleyh.cooney@gmail.com>
2021-07-09 12:41:43 +0000

    (feat): Render packages to html in frontend

--- packages/views.py
+++ packages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Package
 
 # Create your views here.
 
 def package_index(request):
-    return render(request, 'packages/packages.html')
+    context = {}
+    packages = Package.objects.all()
+
+    context['packages'] = packages
+
+    return render(request, 'packages/packages.html', context)
