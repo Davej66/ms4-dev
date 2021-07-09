9936b0d7c0e1698e5a4f1af0123900ad7baafcef
bradleyhc <bradleyh.cooney@gmail.com>
2021-07-09 14:52:17 +0000

    (feat): Add package selection to session and context processor

--- checkout/contexts.py
+++ checkout/contexts.py
@@ -1,16 +1,22 @@
 
 
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from packages.models import Package
 
 
 def order_summary(request):
 
-    package_selected = 0  # pk of the package item
+    package_selected = request.session['package_selection']
     package_cost = 0
+
+    package = get_object_or_404(Package, pk=package_selected['package_id'])
+    print(package_selected)
 
     context = {
         'package_selected': package_selected,
-        'package_cost': package_cost
+        'package_cost': package_cost,
+        'package': package
     }
 
     return context
