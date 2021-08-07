7eae416844480acfae5cf2d6787cff7eab1eb7a3
bradleyhc <bradleyh.cooney@gmail.com>
2021-08-07 21:40:49 +0000

    (fix): Fix env variables in dev and production environments and update dependencies in requirements.txt

--- freelanceMeetup/settings.py
+++ freelanceMeetup/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-
 import users
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-# SECRET_KEY = ("q^k#!6odky-m^yb7^)_sm1i&c&3^4!l)xwk*zqu)90ph4n+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ.get('DEVELOPMENT')
-# DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bc-freelance-meetups.herokuapp.com', 'localhost']
 
