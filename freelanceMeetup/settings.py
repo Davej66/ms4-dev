463071f380692f8268031ce48d850ef051a7dc79
bradleyhc <bradleyh.cooney@gmail.com>
2021-08-07 21:06:25 +0000

    (fix): Update 'Allowed Host' to correct URL for Heroku deployment

--- freelanceMeetup/settings.py
+++ freelanceMeetup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['bc-freelance-meetups.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://bc-freelance-meetups.herokuapp.com/', 'localhost']
 
 
 # Application definition
