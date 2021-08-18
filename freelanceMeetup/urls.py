7a2a6c77941156c048c23c867ba75b59a2733cef
Bradley Cooney <bradleyh.cooney@gmail.com>
2021-08-18 22:41:48 +0100

    (fix): Update packages.html and remove beginning '/' potentially causing 400 error on Heroku

--- freelanceMeetup/urls.py
+++ freelanceMeetup/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('profile/', include('users.urls')),
     path('packages/', include('packages.urls')),
     path('checkout/', include('checkout.urls')),
-    path('', include('users.urls')),
     path('', include('events.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
