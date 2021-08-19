58fa1bc60506b992fd247bcce712d12235332c3b
bradleyhc <bradleyh.cooney@gmail.com>
2021-08-19 13:34:31 +0000

    (feat): Add logic to packages.py to show user their current package and upgrade options if account and package exists

--- packages/views.py
+++ packages/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from .models import Package
+from users.models import MyAccount
 
 # Create your views here.
 
 def package_index(request):
     context = {}
     packages = Package.objects.all()
+
+    print(request.user)
+
+    # Check if user authenticated & show current package if yes
+    if request.user.is_authenticated:
+        user = MyAccount.objects.get(email=request.user)
+        users_package = Package.objects.get(tier=user.package_tier)
+
+        if user.package_tier:
+            context = {
+                'users_package': users_package.tier,
+                'packages': packages
+            }
+            return render(request, 'packages/packages.html', context)    
 
     context['packages'] = packages
 
