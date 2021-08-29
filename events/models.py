e345204197b1c7b8689868f874d9d11f43b5b0f8
bradleyhc <bradleyh.cooney@gmail.com>
2021-08-29 21:03:49 +0000

    (fix): Add event_id to events model to add to header image filename

--- events/models.py
+++ events/models.py
@@ -9,7 +9,7 @@ import pytz
 
 
 def get_header_image_filepath(self, filename):
-    return f'header_images/event_id_{self.primary_key}/header_img.png'
+    return f'header_images/event_id_{self.event_id}/header_img.png'
 
 # Image from Pixabay Image by 
 # https://pixabay.com/users/wanderercreative-855399/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=973460">Stephanie Edwards</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=973460
@@ -17,6 +17,7 @@ def get_default_header_image():
     return 'profile_imgs/default_avatar.png'
 
 class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, blank=False, default="")
     description = models.TextField(max_length=1000, blank=False, default="")
     header_image = models.ImageField(upload_to=get_header_image_filepath, null=True, blank=True, default=get_default_header_image)
